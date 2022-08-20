6e558596317856942ecbe444cf8aa6c0951f843f
chiragkhimani <khimanichirag@gmail.com>
2022-08-20 23:25:18 +0530

    DBUtils

--- src/test/java/com/automation/stepdef/Hooks.java
+++ src/test/java/com/automation/stepdef/Hooks.java
@@ -5,6 +5,7 @@ import com.automation.pages.HomePage;
 import com.automation.pages.LoginPage;
 import com.automation.pages.VacanyListingPage;
 import com.automation.utils.ConfigReader;
+import com.automation.utils.DataBaseUtils;
 import com.automation.utils.DriverUtils;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -12,13 +13,15 @@ import io.cucumber.java.Before;
 public class Hooks {
 
     @Before
-    public void setUp(){
+    public void setUp() throws Exception {
         ConfigReader.initProperties();
         DriverUtils.createDriver();
+        DataBaseUtils.initDatabase();
     }
 
     @After
-    public void quit(){
+    public void quit() throws Exception {
         DriverUtils.getDriver().quit();
+        DataBaseUtils.closeConnection();
     }
 }
--- src/test/java/com/automation/utils/DataBaseUtils.java
+++ src/test/java/com/automation/utils/DataBaseUtils.java
@@ -6,22 +6,24 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class DataBaseUtils {
-    public static void main(String[] args) throws Exception {
-        // 1. Connect to the Database
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/companydb", "root", "Trust@123");
+    static Connection con;
 
+    public static void initDatabase() throws Exception {
+        // 1. Connect to the Database
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/companydb", "root", "Trust@123");
+    }
+
+    public static ResultSet executeQuery(String query) throws Exception{
         // 2. Query the Database
         Statement stmt = con.createStatement();
-        ResultSet result = stmt.executeQuery("select first_name from employees;");
+        ResultSet result = stmt.executeQuery(query);
+        return result;
+    }
 
-        // 3. Process the result / Print the result
-        while(result.next()){
-            System.out.println(result.getString(1));
-        }
-
+    public static void closeConnection() throws Exception{
         // 4. Close the connection
         con.close();
-
     }
+
 }
